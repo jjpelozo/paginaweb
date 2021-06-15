1d0fefa98e91238fc531639241d1b8a2e77ac859
Jesus <jesusjpelozo@gmail.com>
2021-06-15 12:30:41 -0300

    cambios header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
 import { Link } from "react-scroll";
 function Header() {
     return (
-        <div className="md-12">
-            <div className="header__logo">
-
-            </div>
+        <div className="">
+               <div className="header__logo">
+            <img src="logo_transparent.png" alt="" className="h-100 w-100"/>
+        </div>
             <div className='text-center mb-5'>
             <Link to="proyectos" smooth duration={1000}>
                 <a className="btn btn-outline-light">Ver mas</a>
